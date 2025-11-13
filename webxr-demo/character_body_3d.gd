cdaea4e4b0db9c2282130d287a17cafb82c3987e
jandus22 <janek3014@gmail.com>
2025-11-13 14:04:54 +0100

    oby dziallao

--- webxr-demo/character_body_3d.gd
+++ webxr-demo/character_body_3d.gd
@@ -7,13 +7,13 @@ extends CharacterBody3D
 var speed := 3.0
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	input_vector.y = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
-	input_vector = input_vector.normalized()
+    var input_vector = Vector2.ZERO
+    input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+    input_vector.y = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+    input_vector = input_vector.normalized()
 
-	var direction = (head.global_transform.basis * Vector3(input_vector.x, 0, input_vector.y)).normalized()
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+    var direction = (head.global_transform.basis * Vector3(input_vector.x, 0, input_vector.y)).normalized()
+    velocity.x = direction.x * speed
+    velocity.z = direction.z * speed
 
-	move_and_slide()
+    move_and_slide()
